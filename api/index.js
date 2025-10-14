d8c952cfaa8c758a8c39f4a7357ddacc05257258
Willian <williamsilva1942@gmail.com>
2025-10-14 12:02:52 -0300

    chore: update TypeScript build configuration and remove unused API entry point
    - Added "api" to the exclude list in tsconfig.build.json to prevent unnecessary compilation.
    - Deleted the api/index.ts file as it is no longer needed.

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const { NestFactory } = require('@nestjs/core');
+const { SwaggerModule } = require('@nestjs/swagger');
+const { patchNestJsSwagger, ZodValidationPipe } = require('nestjs-zod');
+const { AppModule } = require('../dist/src/app.module');
+const { validateEnv } = require('../dist/src/config/env.validation');
+const { createLoggerConfig } = require('../dist/src/config/logger.config');
+const { createSwaggerConfig, SWAGGER_SETUP_OPTIONS } = require('../dist/src/config/swagger.config');
+const { createCorsConfig } = require('../dist/src/config/cors.config');
+
+const API_PREFIX = 'api/v1';
+const DOCS_PATH = 'api/docs';
+
+let cachedApp;
+
+async function createApp() {
+  if (cachedApp) {
+    return cachedApp;
+  }
+
+  const env = validateEnv(process.env);
+
+  const app = await NestFactory.create(AppModule, {
+    logger: createLoggerConfig(env),
+  });
+
+  app.enableCors(createCorsConfig());
+  app.setGlobalPrefix(API_PREFIX);
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  // Setup Swagger
+  patchNestJsSwagger();
+  const config = createSwaggerConfig();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(DOCS_PATH, app, document, SWAGGER_SETUP_OPTIONS);
+
+  await app.init();
+
+  cachedApp = app;
+  return app;
+}
+
+module.exports = async function handler(req, res) {
+  const app = await createApp();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return expressApp(req, res);
+};
+
